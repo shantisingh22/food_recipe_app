d423b362187f4bb1590bf78b0f8c932b88127f9d
shantisingh <shantisingh22@navgurukul.org>
2024-07-13 10:43:13 +0530

    working on Home page

--- app/page.js
+++ app/page.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import DummyData from './recipeData'; // Ensure this path is correct
+import React from "react";
+import DummyData from './recipeData';
+import "./globals.css";
 
-function Page() {
+const FoodList = () => {
   return (
-    <div>
-      <h1>Food Recipe</h1>
-      <div>
-        {DummyData.map((recipe) => (
-          <div key={recipe.id}>
-            <h2>{recipe.name}</h2>
-            <img src={recipe.image} alt={recipe.name} />
-            <p>{recipe.description}</p>
+    <div className="container">
+      <h2>What's on your mind?</h2>
+      <div className="row flex-row flex-nowrap overflow-auto">
+        {DummyData.map((item) => (
+          <div className="col-auto food-item text-center" key={item.id}>
+            <img src={item.image} alt={item.name} className="image img-fluid rounded-circle" />
+            <h5>{item.name}</h5>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
-<<<<<<< HEAD
-export default Page;
-=======
-export default Page;
->>>>>>> 43dd5bfe11a75d86a03ff1f6a05976340d6aa2ef
+export default FoodList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import DummyData from './recipeData'; // Ensure this path is correct
+
+// function Page() {
+//   return (
+//     <div>
+//       <h1>Food Recipe</h1>
+//       <div>
+//         {DummyData.map((recipe) => (
+//           <div key={recipe.id}>
+//             <h2>{recipe.name}</h2>
+//             <div className="recipe-image-div">
+//             <img className="recipe-image" src={recipe.image} alt={recipe.name} />
+//             </div>
+//             <p>{recipe.description}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+// export default Page;
